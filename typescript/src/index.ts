d8c15d274372c4d4ea0ba4c6750f202589a053c9
nogal2 <shtmdgus@gmail.com>
2022-04-04 23:19:16 +0900

    동기와 비동기
    ㅇ

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,34 +1,33 @@
-function taskA(a,b, cb) {
+function taskA(a: number,b: number, cb: (res:number)=> void):void {
     setTimeout(() => {
       const res = a+b
       cb(res)  
     }, 3000)
 }
 
-function taskB(a,cb) {
+function taskB(a:number,cb:(res:number)=> void):void {
     setTimeout(() => {
        const res = a * 2
        cb(res) 
     }, 1000);
 }
 
-function  taskC(a, cb) {
+function  taskC(a:number, cb:(res:number)=> void):void {
     setTimeout(() => {
         const res = a * -1
         cb(res)
     }, 2000)
 }
 
-taskA(3,4, (res) => {
-    console.log("A TASK RESULT: ", res) 
+taskA(4,5, (a_res) => {
+    console.log("A Result", a_res)
+    taskB(a_res, (b_res) => {
+        console.log("B Result", b_res)
+        taskC(b_res, (c_res) => {
+            console.log("C Result", c_res)
+        })
+    })
 })
 
-taskB(7, (res) => {
-    console.log("B TASK RESULT: ", res)
-})
-
-taskC(14, (res) => {
-    console.log("C TASK RESULT: ", res)
-})
 console.log("코드 끝")
 
---
+++ typescript/src/sync&Async.ts
@@ -0,0 +1,68 @@
+/* function taskA(a: number,b: number, cb: (res:number)=> void):void {
+    setTimeout(() => {
+      const res = a+b
+      cb(res)  
+    }, 3000)
+}
+
+function taskB(a:number,cb:(res:number)=> void):void {
+    setTimeout(() => {
+       const res = a * 2
+       cb(res) 
+    }, 1000);
+}
+
+function  taskC(a:number, cb:(res:number)=> void):void {
+    setTimeout(() => {
+        const res = a * -1
+        cb(res)
+    }, 2000)
+}
+
+taskA(3,4, (res) => {
+    console.log("A TASK RESULT: ", res) 
+})
+
+taskB(7, (res) => {
+    console.log("B TASK RESULT: ", res)
+})
+
+taskC(14, (res) => {
+    console.log("C TASK RESULT: ", res)
+})
+console.log("코드 끝")
+
+ */
+
+function taskA(a: number,b: number, cb: (res:number)=> void):void {
+    setTimeout(() => {
+      const res = a+b
+      cb(res)  
+    }, 3000)
+}
+
+function taskB(a:number,cb:(res:number)=> void):void {
+    setTimeout(() => {
+       const res = a * 2
+       cb(res) 
+    }, 1000);
+}
+
+function  taskC(a:number, cb:(res:number)=> void):void {
+    setTimeout(() => {
+        const res = a * -1
+        cb(res)
+    }, 2000)
+}
+
+taskA(4,5, (a_res) => {
+    console.log("A Result", a_res)
+    taskB(a_res, (b_res) => {
+        console.log("B Result", b_res)
+        taskC(b_res, (c_res) => {
+            console.log("C Result", c_res)
+        })
+    })
+})
+
+console.log("코드 끝")

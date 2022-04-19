0927dbcf391bd4ed894150c9007a6bead5100729
nogal2 <shtmdgus@gmail.com>
2022-04-19 09:13:58 +0900

    백준문제풀이
    ㅇ

---
+++ baekjoon/bj2562(최댓값,인덱스).js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split('\n').map(val => Number(val))
+const max = Math.max(...input)
+console.log(max)
+console.log(input.indexOf(max)+1)

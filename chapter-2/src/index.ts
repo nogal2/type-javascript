0727c0d8a621961151dfb9238b88bb7e04979dd4
nogal2 <94533750+nogal2@users.noreply.github.com>
2022-04-20 20:54:23 +0900

    백준 문제

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,6 +1,44 @@
-let a = true
-var b = false
-const c = true
-let d:boolean = true
-let e:true = true
-let f:true = false
+type Cat = {name:string, purrs:boolean}
+type Dog = {name:string, barks:boolean, wags:boolean}
+type CatOrDogOrBoth = Cat | Dog
+type CatAndDog = Cat & Dog
+
+let a:CatOrDogOrBoth = {
+    name:'Bon',
+    purrs: true
+}
+
+a = {
+    name: 'Domino',
+    barks: true,
+    wags: true
+}
+
+a = {
+    name: 'Donkers',
+    barks: true,
+    purrs: true,
+    wags: true
+}
+
+let b: CatAndDog = {
+    name: 'Domino',
+    barks: true,
+    purrs: true,
+    wags: true,
+}
+
+b = {
+    name:'Dog',
+    purrs: true
+}
+
+function trueOrNull(isTrue: boolean) {
+    if(isTrue) return 'true'
+    return null
+}
+type Returns = string | null
+
+function re(a:string, b:number) {
+    return a || b   // 조건이 참이면 string 아니면 number
+}

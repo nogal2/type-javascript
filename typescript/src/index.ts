285a746027166ffc6f6ce57433dc8c136d996bb8
nogal2 <94533750+nogal2@users.noreply.github.com>
2022-04-22 10:14:32 +0900

    Update index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,25 @@
-const fs = require('fs')
-const input = Number(fs.readFileSync('/dev/stdin').toString())
+function solution(id_list, report, k) {
+    let answer = new Array(id_list.length)
+    answer.fill(0)
 
-for(let i=1; i < 10; i++) {
-    console.log(`${input} * ${i} = ${input*i}`)
+    const reported:any = {}
+
+    id_list.map((user) => {
+        reported[user] = []
+    })
+
+    report.map((user) => {
+        const [userId, reportId] = user.split(' ')
+        if(!reported[reportId].includes(userId)) {
+            reported[reportId].push(userId)
+        }
+    })
+
+    for(const key in reported) {
+        if(reported[key] >= k) {
+            reported.map((user) => {
+                answer[id_list.indexOf(user)] += 1
+            })
+        }
+    }
 }

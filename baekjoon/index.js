ba624dfde4b239c876b4a70cda1228da98157310
nogal2 <94533750+nogal2@users.noreply.github.com>
2022-04-28 21:13:42 +0900

    자바스크립트

--- baekjoon/index.js
+++ baekjoon/index.js
@@ -1,29 +1,13 @@
-// 내 풀이
-function solution(s) {
-  let answer = s;
+var foo = new Object();
 
-  // prettier-ignore
-  const arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+foo.name = "foo";
+foo.age = 30;
+foo.gender = "male";
 
-  for (let i = 0; i < arr.length; i++) {
-    let regex = new RegExp(arr[i], "g");
-    answer = answer.replace(regex, i);
-  }
-
-  const result = parseInt(answer);
-  return result;
-}
-
-// 다른사람풀이
-function solution(s) {
-  // prettier-ignore
-  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-  var answer = s;
-
-  for (let i = 0; i < numbers.length; i++) {
-    let arr = answer.split(numbers[i]);
-    answer = arr.join(i);
-  }
-
-  return Number(answer);
-}
+console.log(typeof foo);
+console.log(foo);
+var foo = {
+  name: "foo",
+  age: 30,
+  gender: "male",
+};

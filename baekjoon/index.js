fb3e799c3186ecb1e5ae5e0784b1eb026cec7473
nogal2 <94533750+nogal2@users.noreply.github.com>
2022-04-30 00:23:04 +0900

    Update index.js

--- baekjoon/index.js
+++ baekjoon/index.js
@@ -1,11 +1,57 @@
 //const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-const input = [1];
+const input = [
+  "14",
+  "push 1",
+  "push 2",
+  "top",
+  "size",
+  "empty",
+  "pop",
+  "pop",
+  "pop",
+  "size",
+  "empty",
+  "pop",
+  "push 3",
+  "empty",
+  "top",
+];
+
 const numbers = parseInt(input[0]);
 const stack = [];
-const command = ["push", "pop", "size", "empty", "top"];
-//console.log(command.includes("pp"));
-for (let i = 1; i < numbers; i++) {
-  if(!command.includes(input[i]) {
-    
+const result = [];
+//const command = ["push", "pop", "size", "empty", "top"];
+for (let i = 1; i < numbers + 1; i++) {
+  /*if (!command.includes(input[i])) {
+    const result = input[i].split(" ");
+    stack.push(Number(result[1]));
+  } */
+  switch (input[i]) {
+    case "pop":
+      {
+        if (stack.length === 0) result.push(-1);
+        else result.push(stack.pop());
+      }
+      break;
+
+    case "size":
+      result.push(stack.length);
+      break;
+
+    case "empty":
+      result.push(stack[0] ? 0 : 1);
+      break;
+
+    case "top":
+      {
+        if (stack.length === 0) result.push(-1);
+        else result.push(stack[stack.length - 1]);
+      }
+      break;
+
+    default:
+      stack.push(input[i].split(" ")[1]);
+      break;
   }
 }
+console.log(result.join("\n"));

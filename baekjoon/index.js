841f411fededa1b6bd7c4c208c149a40e1ebfde3
nogal2 <94533750+nogal2@users.noreply.github.com>
2022-05-23 22:25:46 +0900

    자바스크립트

--- baekjoon/index.js
+++ baekjoon/index.js
@@ -1,56 +1,11 @@
-// 내풀이 틀림;
-function solution(n, lost, reserve) {
-  let answer = 0;
-  const newReserve = [];
-  reserve.map((val) => {
-    lost.includes(val) ? {} : newReserve.push(val);
-  });
-
-  if (lost[0] === 1) {
-    lost.map((val) => {
-      return newReserve.includes(val + 1) ? (answer += 1) : (answer = answer);
-    });
-  } else {
-    lost.map((val) => {
-      return newReserve.includes(val - 1) ? (answer += 1) : (answer = answer);
-    });
-  }
-
-  if (answer === lost.length) {
-    answer = n;
-  } else {
-    answer = n - answer;
-  }
-
-  return answer;
+function Person(name, age, hobby) {
+  this.name = name
+  this.age = age
+  this.hobby = hobby
 }
 
-// 다른사람풀이
+let foo = new Person('foo', 30, 'tennis')
 
-function solution(n, lost, reserve) {
-  var answer = n - lost.length;
-  // 처음 가능한 학생수 = n - lost.length
-  // lost 배열 앞뒤의 값을 reserve에 포함되어있는지를 확인 -> 해당값을 reserve에서 뺌 + answer++
-  // 왜 정렬을 해줘야 하지? - [4,2], [3,5]와 같은 케이스 때문
+console.log(foo.hasOwnProperty('name'));
 
-  let realLost = lost.filter((l) => !reserve.includes(l));
-  let realReserve = reserve.filter((r) => !lost.includes(r));
-  answer += lost.length - realLost.length;
-
-  realLost.sort((a, b) => a - b);
-
-  realLost.forEach((l) => {
-    if (realReserve.length === 0) {
-      return;
-    }
-
-    if (realReserve.includes(l - 1)) {
-      realReserve = realReserve.filter((r) => r !== l - 1);
-      answer++;
-    } else if (realReserve.includes(l + 1)) {
-      realReserve = realReserve.filter((r) => r !== l + 1);
-      answer++;
-    }
-  });
-  return answer;
-}
+console.dir(Person.prototype)
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,25 +1,17 @@
-type Mapp = <T, U>(array: T[], f: (item: T) => U) => U[];
-
-function map(array: unknown[], f: (item: unknown) => unknown): unknown[] {
-  let result = [];
-  for (let i = 0; i < array.length; i++) {
-    result[i] = f(array[i]);
-  }
-  return result;
+let c: {
+  firstName: string
+  lastName: string
+} = {
+  firstName: 'john',
+  lastName: 'barrowman'
 }
 
-let promise = new Promise<number>((resolve) => resolve(45));
-
-promise.then((result) => console.log(result * 4));
-
-type MyEvent<T> = {
-  target: T;
-  type: string;
-};
-
-let a = 0;
-
-if (true) {
-  let a = 1;
+class Person {
+  constructor(
+    public firstName: string,
+    public lastName: string
+  ) {}
 }
-console.log(a); // 0
+
+c = new Person('matt', 'smith')
+console.log(c)
